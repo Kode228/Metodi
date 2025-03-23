cc81daa440f9349ce3ab79f4cc698f87050b81eb
Kode228 <fpam00873@gmail.com>
2025-03-23 11:56:39 +0900

    16.12.2024

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void reverseCopy(int* source, int* destination, int size) {
+    int* sourceEnd = source + size - 1;
+    int* destStart = destination;
+
+    for (int i = 0; i < size; i++)
+    {
+        *destStart = *sourceEnd;
+        destStart++;
+        sourceEnd--;
+
+    }
+
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int sourceArray[] = { 1, 2, 3, 4, 5 };
+    int size = sizeof(sourceArray) / sizeof(sourceArray[0]);
+    int* destinationArray = new int[size]; 
+
+    reverseCopy(sourceArray, destinationArray, size);
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < size; ++i) {
+        cout << *(sourceArray + i) << " ";
+    }
+    cout << endl;
+
+    cout << "Обращенный массив: ";
+    for (int i = 0; i < size; ++i) {
+        cout << *(destinationArray + i) << " ";
+    }
+    cout << endl;
+
+    delete[] destinationArray; 
+
+    return 0;
+}
